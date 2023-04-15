0cda9964ea0cb4b4930dc277cbba29921fae19a0
kehindeBankole <bankolek1@gmail.com>
2023-04-15 06:39:00 +0100

    show blocker screen

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -10,53 +10,64 @@ const isFailed = ref(false);
 function selectWrongAnswer() {
   isFailed.value = true;
 }
-function next(){
+function next() {
   if (count.value === questions?.length - 1) {
-      alert(`congratulations you won ${scores[count.value]}`);
-    } else {
-      count.value += 1;
-    }
+    alert(`congratulations you won ${scores[count.value]}`);
+  } else {
+    count.value += 1;
+  }
 }
 </script>
 
 <template>
-  <div v-if="!isFailed" class="grid grid-cols-12 h-screen">
-    <div class="bg-red-500 col-span-10 grid grid-rows-[6fr_6fr]">
-      <div class="bg-white grid place-items-center">
-        <p v-if="count - 1 >= 0">{{ scores[count - 1] }}</p>
-      </div>
-      <div class="bg-blue-900">
-        <div class="mt-[-3rem]">
-          <div class="trapezoid text-white relative grid place-items-center">
-            <p class="translate-y-10 z-[99] text-xl">
-              {{ questions[count].text }}
-            </p>
+  <main class="hidden lg:block">
+    <div v-if="!isFailed" class="grid grid-cols-12 h-screen">
+      <div class="bg-red-500 col-span-10 grid grid-rows-[6fr_6fr]">
+        <div class="bg-white grid place-items-center">
+          <p v-if="count - 1 >= 0">{{ scores[count - 1] }}</p>
+        </div>
+        <div class="bg-blue-900">
+          <div class="mt-[-3rem]">
+            <div class="trapezoid text-white relative grid place-items-center">
+              <p class="translate-y-10 z-[99] text-xl">
+                {{ questions[count].text }}
+              </p>
+            </div>
+            <div class="inverted-trapezoid"></div>
           </div>
-          <div class="inverted-trapezoid"></div>
+          <div class="mt-10">
+            <Options
+              :questions="questions[count]"
+              :select-wrong-answer="selectWrongAnswer"
+              :next="next"
+            />
+          </div>
         </div>
-        <div class="mt-10">
-          <Options
-            :questions="questions[count]"
-            :select-wrong-answer="selectWrongAnswer"
-            :next="next"
-          />
+      </div>
+      <div class="bg-blue-500 col-span-2">
+        <div class="grid grid-rows-[2fr_10fr]">
+          <div class="bg-blue-800">
+            <h1 class="text-white">LIFELINES</h1>
+          </div>
+          <div class="px-6">
+            <Score :current-position="count" />
+          </div>
         </div>
       </div>
     </div>
-    <div class="bg-blue-500 col-span-2">
-      <div class="grid grid-rows-[2fr_10fr]">
-        <div class="bg-blue-800">
-          <h1 class="text-white">LIFELINES</h1>
-        </div>
-        <div class="px-6">
-          <Score :current-position="count" />
-        </div>
-      </div>
+    <div v-if="isFailed">
+      <WrongAnswer
+        :info="
+          count > 0
+            ? `you won only ${scores[count - 1]} `
+            : 'you did not win any money, go home'
+        "
+      />
     </div>
-  </div>
-  <div v-if="isFailed">
-    <WrongAnswer :info="count>0 ? `you won only ${scores[count-1]} ` : 'you did not win any money, go home'"/>
-  </div>
+  </main>
+  <main class="grid place-items-center lg:hidden h-screen">
+    <p>PLEASE PAY ON A DESKTOP DEVICE 🙏🏿</p>
+  </main>
 </template>
 
 <style>
